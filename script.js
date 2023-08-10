5a25f58db88f065aa59ce2ab3dce089a3b5d9cd3
Lara Drews <127211335+laraDrews@users.noreply.github.com>
2023-08-10 08:17:56 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,16 @@
+function novaPg(){
+    window.location.href="a.html"
+
+}
+function novaPag(){
+    window.location.href="b.html"
+
+}
+function novaPaag(){
+    window.location.href="c.html"
+
+}
+function novaPaaag(){
+    window.location.href="d.html"
+
+}
